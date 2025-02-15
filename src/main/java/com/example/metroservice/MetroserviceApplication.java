49c3d09ee3779188b6a7b8880af11e40df596462
Lakshmi MJ <mjlakshmi66@gmail.com>
2025-02-15 21:34:04 +0530

    initial commit

---
+++ src/main/java/com/example/metroservice/MetroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.metroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetroserviceApplication.class, args);
+	}
+
+}
